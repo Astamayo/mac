f0d309ab6bb7bb75dfc6f363944db759ed30d1f7
antoniosilverio <antoniosilverio@localhost>
2024-04-25 20:07:46 -0400

    Update with class calling

--- maven-example-jar/src/main/java/com/maintlog/maven/classes/App.java
+++ maven-example-jar/src/main/java/com/maintlog/maven/classes/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        myClass calc = new myClass();
+        int output;
+        output = calc.calculator(2, 3);
+        System.out.println(output);
+        
     }
 }
--- maven-example-jar/src/main/java/com/maintlog/maven/classes/myClass.java
+++ maven-example-jar/src/main/java/com/maintlog/maven/classes/myClass.java
@@ -1,5 +1,12 @@
 package com.maintlog.maven.classes;
 
 public class myClass {
+	int c;
+
+	public int calculator(int a, int b) {
+		c = a * b;
+
+		return c;
+	}
 
 }
